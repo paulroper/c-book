642615d4500dc1ec2967408fbd93c293c98948ba
Paul Roper <paul.roper@red-badger.com>
2018-07-19 10:50:49 +0100

    Add section 1.2 exercise

---
+++ src/1.2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table
+ for fahr = 0, 20, ..., 300; floating-point version */
+int main(int argc, char const *argv[])
+{
+  float fahr, celsius;
+  float lower, upper, step;
+  lower = 0;   /* lower limit of temperatuire scale */
+  upper = 300; /* upper limit */
+  step = 20;   /* step size */
+  fahr = lower;
+
+  /* 1-3 */
+  printf("Fahrenheit Celsius\n");
+  while (fahr <= upper)
+  {
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%3.0f %6.1f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+
+  printf("\n");
+  celsius = lower;
+
+  /* 1-4 */
+  printf("Celsius Fahrenheit\n");
+  while (celsius <= upper)
+  {
+    fahr = (celsius * 1.8) + 32.0;
+    printf("%3.0f %6.1f\n", celsius, fahr);
+    celsius = celsius + step;
+  }
+}
