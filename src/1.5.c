309e3bbf5bf8c5e519d328941a0a59390a6609ea
Paul Roper <paul.roper@red-badger.com>
2018-07-19 12:05:31 +0100

    Add section 1.5 exercise

---
+++ src/1.5.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 1
+
+int count_input(void);
+int replace_blanks(void);
+int replace_tabs_backspaces(void);
+
+int count_input()
+{
+  int c, lines, spaces, tabs;
+
+  lines = 0;
+  spaces = 0;
+  tabs = 0;
+
+  while ((c = getchar()) != EOF)
+  {
+    if (c == '\n')
+    {
+      ++lines;
+    }
+    else if (c == ' ')
+    {
+      ++spaces;
+    }
+    else if (c == '\t')
+    {
+      ++tabs;
+    }
+  }
+
+  printf("Lines: %d\n", lines);
+  printf("Spaces: %d\n", spaces);
+  printf("Tabs: %d\n", tabs);
+
+  return 0;
+}
+
+int replace_blanks()
+{
+  int current_char;
+
+  while ((current_char = getchar()) != EOF)
+  {
+    if (current_char == ' ')
+    {
+      while ((current_char = getchar()) == ' ')
+        ;
+      putchar(' ');
+    }
+
+    putchar(current_char);
+  }
+
+  return 0;
+}
+
+int replace_tabs_backspaces()
+{
+  int current_char;
+
+  while ((current_char = getchar()) != EOF)
+  {
+    if (current_char == '\t')
+    {
+      putchar('\\');
+      putchar('t');
+    }
+    else if (current_char == '\b')
+    {
+      putchar("\\");
+    }
+
+    putchar(current_char);
+  }
+}
+
+int main(int argc, char const *argv[])
+{
+  int current_char, next_char;
+
+  while ((current_char = getchar()) != EOF)
+  {
+    putchar(current_char);
+
+    while ((current_char = getchar()) != ' ')
+    {
+      if (current_char == EOF)
+      {
+        break;
+      }
+
+      putchar(current_char);
+    }
+
+    putchar('\n');
+  }
+}
